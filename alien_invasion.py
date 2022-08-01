c7a29240a5bc86b4f0a88e7dedd40922e8a494ce
xiangmy21 <2248278431@qq.com>
2022-08-01 17:14:54 +0800

    外星人群已显示

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -19,6 +20,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -70,13 +74,43 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-    
+
+    def _create_fleet(self):
+        """创建外星人群"""
+        # 创建一个外星人
+        alien = Alien(self)
+        
+        # 计算一行可容纳多少外星人
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - 2 * alien_width
+        numbers_alien_x = available_space_x // (2 * alien_width)
+        
+        # 计算可容纳多少行外星人
+        available_space_y = (self.settings.screen_height - 3 * alien_height
+                                - self.ship.rect.height)
+        numbers_rows = available_space_y // (2 * alien_height)
+
+        # 创建外星人群
+        for row_number in range(numbers_rows):
+            for alien_number in range(numbers_alien_x):
+                self._create_alien(alien_number, row_number)
+            
+    def _create_alien(self, alien_number, row_number):
+        """根据行列位置创建一个外星人"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """更新屏幕图像，并刷新屏幕"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
