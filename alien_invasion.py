b5d622ff7399e9904232a9fa03d678bc10a0597e
xiangmy21 <2248278431@qq.com>
2022-08-01 14:19:06 +0800

    成功显示飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,22 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -18,6 +26,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # 重新设定屏幕
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # 显示最新绘制的屏幕
             pygame.display.flip()
 
